413fad367fe4f345ced7c3a2defd8266eeed63a7
Analia <ana_292_1@hotmail.com>
2024-06-19 21:37:54 -0300

    funcionalidad local storage

--- src/App.jsx
+++ src/App.jsx
@@ -2,13 +2,19 @@ import "./App.css";
 import { Stack, Typography } from "@mui/material";
 import { Tarea } from "./componentes/Tarea";
 import { Form } from "./componentes/Form";
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import { v4 as uuidv4 } from "uuid";
 
 function App() {
-	const [arrayTareas, setArrayTareas] = useState([
-		{ id: uuidv4(), descripcion: "Tarea de ejemplo" },
-	]);
+	const [arrayTareas, setArrayTareas] = useState(
+		JSON.parse(localStorage.getItem("arrayTareas")) || [
+			{ id: uuidv4(), descripcion: "Tarea de ejemplo" },
+		]
+	);
+
+	useEffect(() => {
+		localStorage.setItem("arrayTareas", JSON.stringify(arrayTareas));
+	}, [arrayTareas]);
 
 	const handleArrayTareas = (descripcionTarea) => {
 		const nuevaTarea = { id: uuidv4(), descripcion: descripcionTarea };
