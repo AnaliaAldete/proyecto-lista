524928f8bfc851eba6bd99ebac01821c20bde14a
Analia <ana_292_1@hotmail.com>
2024-06-20 15:39:44 -0300

    funcionalidad react hook form

--- src/componentes/Form.jsx
+++ src/componentes/Form.jsx
@@ -1,3 +1,5 @@
+//ver error al escribir!!!
+
 import {
 	Stack,
 	TextField,
@@ -7,24 +9,43 @@ import {
 	FormControl,
 	InputLabel,
 } from "@mui/material";
-import { useState } from "react";
+import { useForm } from "react-hook-form";
 
 export const Form = ({ agregarTarea }) => {
-	const [tarea, setTarea] = useState("");
+	const {
+		register,
+		handleSubmit,
+		formState: { errors },
+		reset,
+	} = useForm();
 
-	const handleSubmit = (e) => {
-		e.preventDefault();
-		agregarTarea(tarea);
+	const onSubmit = (data) => {
+		agregarTarea(data.descripcion);
+		reset();
 	};
 	return (
 		<Stack spacing={10} direction="row">
-			<form onSubmit={handleSubmit}>
+			<form onSubmit={handleSubmit(onSubmit)}>
 				<TextField
-					id="tarea"
+					type="text"
+					id="descripcion"
 					label="Agregar una tarea"
 					variant="filled"
-					value={tarea}
-					onChange={(e) => setTarea(e.target.value)} //ver lo del name
+					name="descripcion"
+					{...register("descripcion", {
+						required: "Este campo es obligatorio",
+						minLength: {
+							value: 3,
+							message: "La tarea debe tener al menos 3 caracteres",
+						},
+						maxLength: {
+							value: 60,
+							message: "La tarea no puede tener más de 60 caracteres",
+						},
+						pattern: /\S/,
+					})}
+					error={errors.descripcion ? true : false}
+					helperText={errors.descripcion ? errors.descripcion.message : ""}
 				/>
 				<Button variant="contained" type="submit">
 					Agregar Tarea
@@ -35,9 +56,9 @@ export const Form = ({ agregarTarea }) => {
 					Seleccione un filtro
 				</InputLabel>
 				<Select label="Estado">
-					<MenuItem value="pendiente">Pendiente</MenuItem>
-					<MenuItem value="completada">Completada</MenuItem>
-					<MenuItem value="todas">Todas</MenuItem>
+					<MenuItem>Pendiente</MenuItem>
+					<MenuItem>Completada</MenuItem>
+					<MenuItem>Todas</MenuItem>
 				</Select>
 			</FormControl>
 		</Stack>
